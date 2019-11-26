b704dd2f041bda70913589355c2674633fcbe2c4
Patrick <ck.longo@bol.com.br>
2019-11-26 15:05:15 -0300

    Footer

---
+++ frontend/frontend/src/widgets/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Rodape from '../components/footer'
+
+class Footer extends Component {
+    render() {
+        return (
+            <Rodape />
+        );
+    }
+}
+
+export default Footer;
