e74bf74562d2b0b2a8fcb2934ea8e105c2fe5eda
Patrick <ck.longo@bol.com.br>
2019-11-26 15:05:08 -0300

    Footer

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
-import React from 'react';
-import Footer from './components/footer'
-
-function App() {
-    return <Footer />;
-=======
 import React, { Component, Fragment } from 'react';
 import Header from './widgets/Header';
+import Footer from './widgets/Footer';
 
 class App extends Component {
     render() {
-        return <Header />;
+        return (
+            <Fragment>
+                <Header />
+                <Footer />
+            </Fragment>
+        );
     }
->>>>>>> e3b9fe56f0b368360f23a61135e685c3aca82419
 }
 
 export default App;
