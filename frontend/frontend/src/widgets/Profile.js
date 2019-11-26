de2b0afe52500a59db29e3002e339db45164b308
Giovani Cuenca <giovani.cuenca@gmail.com>
2019-11-26 18:42:56 -0300

    routes

--- frontend/frontend/src/widgets/Profile.js
+++ frontend/frontend/src/widgets/Profile.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
-import { GlobalStyle } from './styles/global'
+import { GlobalStyle } from './styles/global';
 
 class Profile extends Component {
     render() {
         return (
-            <GlobalStyle />
-            <Header />
-            <Footer />
+                <GlobalStyle />
+                <Header />
+                <Footer />
         );
     }
 }
